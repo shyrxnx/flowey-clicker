93fc0e1cb8446bedd730638f6406b8ba11633f62
shyrxnx <84778328+shyrxnx@users.noreply.github.com>
2025-10-16 15:18:04 +0800

    feat: add a working auto-clicker and upgrade clicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from './components/Button'
 import flowey from './assets/flowey.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [clicker, setClicker] = useState(1)
+  const [clickerPrice, setClickerPrice] = useState(5)  
   const [autoClicker, setAutoClicker] = useState(0)
   const [autoClickerPrice, setAutoClickerPrice] = useState(10)
+
+  const counter = () => {
+    if (clicker > 0){
+      setCount(count + clicker);
+    }
+  }
+
+  useEffect(() => {
+    let interval;
+
+    if (autoClicker > 0) {
+      interval = setInterval(() => {
+        setCount(prev => prev + autoClicker);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [autoClicker]);
 
   return (
     <>
@@ -14,31 +34,40 @@ function App() {
           <img 
             src={flowey} width={254} height={254} 
             className="flowey icon" alt="Flowey from the game Undertale" 
-            onClick={() => setCount((count) => count + 1)}
+            onClick={counter}
           />
       </div>
 
-      <h1>{count}<br></br>floweys</h1>
+      <h1>{Number(count).toFixed(2)}<br></br>floweys</h1>
       
       <div className="options">
         <AutoClicker 
           count={count}
           setCount={setCount}
+          clicker={clicker}
+          setClicker={setClicker}
+          clickerPrice={clickerPrice}
+          setClickerPrice={setClickerPrice}          
           autoClicker={autoClicker}
           setAutoClicker={setAutoClicker}
           autoClickerPrice={autoClickerPrice}
           setAutoClickerPrice={setAutoClickerPrice}
         />
       </div>
-      
-      <p className="debug">
-        {`Auto Clicker: ${autoClicker}`}
-      </p>
-      <p className="debug">
-        {`Auto Clicker Price: ${autoClickerPrice}`}
-      </p>
 
-      <hr />
+      <div className='debug-container'>
+        <div className='debug-column'>
+          <p> {` Clicker: ${clicker}`} </p>
+          <p> {`Clicker Price: ${clickerPrice}`} </p> 
+        </div>
+
+        <div className='debug-column'>
+          <p> {`Auto Clicker: ${autoClicker}`} </p>
+          <p> {`Auto Clicker Price: ${autoClickerPrice}`} </p>
+        </div>
+      </div>
+     
+      <hr width={500} />
 
       <p className="creator">
         Made by: Shyrine Salvador
@@ -47,23 +76,42 @@ function App() {
   )
 }
 
-function AutoClicker({count, setCount, autoClicker, setAutoClicker, autoClickerPrice, setAutoClickerPrice}) {
+function AutoClicker({count, setCount, clicker, setClicker, clickerPrice, setClickerPrice, autoClicker, setAutoClicker, autoClickerPrice, setAutoClickerPrice}) {
 
-  const handleBuy = () => {
-    if (count >= autoClickerPrice) {
-      setAutoClicker(autoClicker + 1)
-      setCount(count - autoClickerPrice)
-      setAutoClickerPrice(autoClickerPrice + 2)
+  const handleBuyClicker = () => {
+    if (count >= clickerPrice) {
+      setClicker(clicker + 1)
+      setCount(count - clickerPrice)
+      setClickerPrice(Number((5 * (1.1) ** (clicker + 1)).toFixed(2)))
     }
   }
 
+  const handleBuyAutoClicker = () => {
+    if (count >= autoClickerPrice) {
+      setAutoClicker(autoClicker + 1)
+      setCount(count - autoClickerPrice)
+      setAutoClickerPrice(Number((10 * (1.1) ** (autoClicker + 1)).toFixed(2)))
+    }
+  }
 
   return (
-    <Button 
-      label={`Buy Auto-Clicker (+1/sec) ${autoClickerPrice}`}
-      className='auto-clicker' 
-      btnColor='#6e581e'
-      onClick={handleBuy}/>
+
+    <>
+      <div className='upgrade-container'>
+        <Button 
+          label={`Buy Clicker (${clicker})`}
+          className='clicker' 
+          btnColor='#6e581e'
+          onClick={handleBuyClicker}/>
+
+        <Button 
+          label={`Buy Auto-Clicker (+${autoClicker}/sec)`}
+          className='auto-clicker' 
+          btnColor='#6e581e'
+          onClick={handleBuyAutoClicker}/>
+      </div>
+    </>
+
   )
 }
 
